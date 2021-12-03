72655a1dd8bcbdf605635b3c493c66c61b0e0a55
david salmon <davidyoels@gmail.com>
2021-12-03 10:43:02 +0200

    feat: the user now can add memorial and joys news into the time screen

--- controllers/joys-news.controller.js
+++ controllers/joys-news.controller.js
@@ -1,5 +1,8 @@
 const timeScreenTexts = require("../utils/i18n/he-IL");
-const { readFileContent } = require("../utils/files/file-actions");
+const {
+  readFileContent,
+  writeContentToFile
+} = require("../utils/files/file-actions");
 
 const addJoysNewsScreen = (req, res, next) => {
   res.render("addJoyNewsScreen", {
@@ -13,7 +16,24 @@ const getJoysNews = async (req, res, next) => {
   });
 };
 
+const addJoysNews = async function (req, res, next) {
+  try {
+    const newJosyNews = {
+      name: req.body["new-joys-news"],
+      time: new Date()
+    };
+    const JoysNews = await writeContentToFile(
+      "joys-news",
+      "joysNews",
+      newJosyNews
+    );
+  } catch (ex) {
+    console.log(ex);
+  }
+};
+
 module.exports = {
   addJoysNewsScreen: addJoysNewsScreen,
-  getJoysNews: getJoysNews
+  getJoysNews: getJoysNews,
+  addJoysNews: addJoysNews
 };
