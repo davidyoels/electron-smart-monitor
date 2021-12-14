3b1fb2610e05771382cc2d296697497ac10d6d36
david salmon <davidyoels@gmail.com>
2021-12-15 00:49:07 +0200

    feat(in progress): add all times and memorial and joys news

--- public/js/joys-news.js
+++ public/js/joys-news.js
@@ -4,7 +4,8 @@ const getJoysNews = () => {
     .then((data) => {
       let joys_news_data = "";
       for (let index in data.joysNews) {
-        joys_news_data += `<text>${data.joysNews[index].name}</text>`;
+        joys_news_data += `<text class="assistant-s-2-5">${data.joysNews[index].name}</text><br/>`;
+        joys_news_data += `*`;
       }
       document.getElementById("joys-news-data").innerHTML = joys_news_data;
       document.getElementById("joys-news-data").innerHTML += joys_news_data;
