7aec8d2f970f6fcb4ba9b0a29aea07efd870ed0b
david salmon <davidyoels@gmail.com>
2021-12-31 14:11:57 +0200

    add circle text on header

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "vendor")));
 
 // Add headers before the routes are defined
 app.use(function (req, res, next) {
